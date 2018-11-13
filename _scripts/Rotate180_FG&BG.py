1d41960151a6b14bac838eae987143419b66ea23
Ryan Bugden <rbugden@gmail.com>
2018-11-13 20:46:11 +0100

    Simplified the script because case-smart rotation was silly.

--- _scripts/Rotate180_FG&BG.py
+++ _scripts/Rotate180_FG&BG.py
@@ -21,18 +21,7 @@ def rotate180BothLayers(centerX, centerY):
 midpointX = glyph.bounds[0] + (glyph.bounds[2] - glyph.bounds[0]) / 2
 midpointY = glyph.bounds[1] + (glyph.bounds[3] - glyph.bounds[1]) / 2
 
-if glyph.unicode == None:
-    rotate180BothLayers(midpointX, midpointY)
-
-else:    
-    character = chr(glyph.unicode)
-
-    if character == character.upper():
-        rotate180BothLayers(glyph.width/2, font.info.capHeight/2)
-    
-    elif character == character.lower():
-        rotate180BothLayers(glyph.width/2, font.info.xHeight/2)
-
+rotate180BothLayers(midpointX, midpointY)
 
 glyph.performUndo()
 glyph.update()
