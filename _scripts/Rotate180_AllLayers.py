e54eecf32fd2f7fb79ab2771237a79237fcc7ecc
Ryan Bugden <rbugden@gmail.com>
2018-12-13 16:49:11 +0100

    Now supports suffixed glyphs.

--- _scripts/Rotate180_AllLayers.py
+++ _scripts/Rotate180_AllLayers.py
@@ -1,15 +1,21 @@
-# menuTitle: Rotate 180 All Layers
+# menuTitle: Rotate 180 - All Layers
 # shortCut: shift+command+r
 
-# Rotate all layers 180 degrees, within current glyph.
-# Use your preferred shortcut in line 2.
-# 2018/11/12
+'''
+A script that rotates your glyph and all of its layers. 
+It does its best to rotate it in a helpful way, not 
+necessarily just around the body of the contours themselves.
+Use your preferred shortcut in line 2.
+
+Last updated: 2018/12/13
+'''
 
 font = CurrentFont()
 glyph = CurrentGlyph()
 
 # list of layers in the font
 layers = font._get_layerOrder()
+
 
 # function that rotates all layers 180 around a given center point
 def rotate180AllLayers(centerX, centerY):
@@ -26,11 +32,25 @@ def rotate180AllLayers(centerX, centerY):
 midpointX = glyph.bounds[0] + (glyph.bounds[2] - glyph.bounds[0]) / 2
 midpointY = glyph.bounds[1] + (glyph.bounds[3] - glyph.bounds[1]) / 2
 
-glyph.prepareUndo()
+glyph.prepareUndo("Rotate")
 
 # uncommon glyphs rotate around their physical midpoint
 if glyph.unicode == None:
-    rotate180AllLayers(midpointX, midpointY)
+    if "." in glyph.name:
+        glyph_parent_name = glyph.name.split(".")[0]
+        print(glyph_parent_name)
+        print(font[glyph_parent_name].unicode)
+        character = chr(font[glyph_parent_name].unicode)
+
+        if character == character.upper():
+            rotate180AllLayers(glyph.width/2, font.info.capHeight/2)
+    
+        elif character == character.lower():
+            rotate180AllLayers(glyph.width/2, font.info.xHeight/2)
+    
+    # a catch-all        
+    else:
+        rotate180AllLayers(midpointX, midpointY)
 
 # common glyphs rotate around the midpoint of the font's metrics
 # this is beneficial, for example, if you want to rotate a /b around its bowl, not its full height
