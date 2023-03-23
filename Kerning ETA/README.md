7dda68ac1f03e7f9300f8212b9c1f94591fb10fc
Ryan Bugden <rbugden@gmail.com>
2023-03-22 23:06:59 -0400

    Fix #4, support multiple MM windows

--- Kerning ETA/README.md
+++ Kerning ETA/README.md
@@ -3,7 +3,7 @@
 Nope! You're probably not. Install this as a [start-up script](https://robofont.com/documentation/how-tos/setting-up-a-startup-script/?highlight=start-up), and you'll get a nice little message on the top-right of your Metrics Machine extension with an estimated time to expect to be done with your pairlist, at your current rate.
 
 **Note:**
-This tool will only look at your last 20 pairs to make an estimation. If you took more than 60 seconds on a pair, it will start over with the next 20 pairs.
+This tool will only look at your last 20 pairs to make an estimation. If you took more than 60 seconds on a pair, it will start over with the next 20 pairs. Also, if you have more than 1 MM window open, switching windows will restart the time-estimation clock, because you're probably finding a new groove anyway.
 
 **Also:**
 This will display time in 12-hour AM/PM. If you'd like 24-hour time. Set `twenty_four = True` near the beginning of the script.
@@ -13,5 +13,6 @@ This will display time in 12-hour AM/PM. If you'd like 24-hour time. Set `twenty
 Thanks to Tal Leming for pointing me to some MM UI infrastructure.
 
 Ryan Bugden
+23.03.22 Support for multiple MM windows / closing & reopening.
 22.07.28
 ```
--- Kerning ETA/kerning_ETA.py
+++ Kerning ETA/kerning_ETA.py
@@ -28,7 +28,7 @@ def get_average_diff(l):
 
 class KerningETA:
     
-    dataset_size = 20 # how many recent pairs you'll allow in your dataset, from which you will surmise the ETA
+    dataset_size = 100 # how many recent pairs you'll allow in your dataset, from which you will surmise the ETA
     sep = " "
     
     def __init__(self):
@@ -37,30 +37,34 @@ class KerningETA:
         self.pair = ()
         self.pairs = []
         self.mm_open = False
-        self.vanilla_initiated = False
-        self.tool_group = None
-        
+        self.mm_fonts_open = []
+
         addObserver(self, "mmClose", "MetricsMachine.ControllerWillClose")
         addObserver(self, "pairChanged", "MetricsMachine.currentPairChanged") 
-        
+
 
     def mmClose(self, sender):
 
         self.mm_open = False
+
+        if self.f in self.mm_fonts_open:
+            self.mm_fonts_open.remove(self.f)
         
 
     def pairChanged(self, sender):
 
+        self.f = CurrentFont()
+
+        
         # try getting the window
         try:
             from mm4.mmScripting import _getMainWindowControllerForFont, MetricsMachineScriptingError
             import metricsMachine
 
-            # Tal's code for grabbing the UI
-            self.controller = _getMainWindowControllerForFont(self.f)
-            self.edit_view = self.controller.editView
-            self.tool_group = self.edit_view.toolGroup
-            self.prog_ind = self.tool_group.progressIndicator
+            controller = _getMainWindowControllerForFont(self.f)
+            edit_view = controller.editView
+            tool_group = edit_view.toolGroup
+            prog_ind = tool_group.progressIndicator
         
             self.mm_open = True
 
@@ -69,63 +73,78 @@ class KerningETA:
             # print("MetricsMachineScriptingError", se)
             self.mm_open = False
             return
-            
+
+
         # if the window’s open...
         if self.mm_open == True:
-            
+
             # change the left pos of the text box depending on whether the progress indicator is present or not
-            x, y, w, h = self.prog_ind.getPosSize()
+            x, y, w, h = prog_ind.getPosSize()
             text_x = x + w + 10
-            if self.prog_ind.isVisible() == False:
+            if prog_ind.isVisible() == False:
                 text_x = x + 10
             
-            # ... and you haven't made the textbox yet
-            if self.vanilla_initiated == False:
-                
-                self.tool_group.message_box = vanilla.TextBox(
+            
+            # add current font to list of fonts open
+            if not self.f in self.mm_fonts_open:
+                self.mm_fonts_open.append(self.f)
+
+                # you haven't made the textbox yet
+                tool_group.message_box = vanilla.TextBox(
                     (text_x, y, -10, 20), 
                     "",
                     alignment='right'
                     )
-                    
-                self.vanilla_initiated = True
 
-            # if you have made the text box, just change the position
             else:
-                self.tool_group.message_box.setPosSize((text_x, y, -10, 20))
+                # if it's not the back of the list (changing windows to another mmFont), reset pair count for time estimate
+                if not self.f == self.mm_fonts_open[-1]:
+                    self.pairs = []
+                    self.updatePairlist(tool_group.message_box)
+
+                # move most recent to the back of the list
+                self.mm_fonts_open.remove(self.f)
+                self.mm_fonts_open.append(self.f)
+
+                # if you have made the text box, just change the position
+                tool_group.message_box.setPosSize((text_x, y, -10, 20))
+
             
-        # get the current pair
-        current_pair = sender["pair"]
-        if current_pair == self.pair:
-            return
-        self.pair = current_pair
+            # get the current pair
+            current_pair = sender["pair"]
+            if current_pair == self.pair:
+                return
+            self.pair = current_pair
+            
+            # get where we are in the pair-list
+            self.pl = metricsMachine.GetPairList()
+            try:
+                p_i = self.pl.index(self.pair)
+                self.pairs_left = len(self.pl) - p_i - 1
+            except ValueError:
+                print(f"Pair {self.pair} is not in the pairlist. Cannot calculate pairs-left in the pairlist.")
+            
+            self.updatePairlist(tool_group.message_box)
         
-        # get where we are in the pair-list
-        self.pl = metricsMachine.GetPairList()
-        p_i = self.pl.index(self.pair)
-        self.pairs_left = len(self.pl) - p_i
-        
-        self.updatePairlist()
-    
-        # calculate time
-        time_per_pair = get_average_diff([b for a, b in self.pairs])
-        time_left = self.pairs_left * time_per_pair
-        ETA = time.time() + time_left
-        if twenty_four == True:
-            ETA = datetime.datetime.fromtimestamp(ETA).strftime('%H:%M')
-        else:
-            ETA = datetime.datetime.fromtimestamp(ETA).strftime('%-I:%M %p')
-        
-        # if you took a break, start the dataset from scratch
-        if time_per_pair > 60:
-            self.pairs = []
-            self.updatePairlist()
-        
-        # set the appropriate info
-        self.tool_group.message_box.set(f"Remaining: {self.pairs_left} {self.sep} Δ: {round(time_per_pair, 1)} sec. {self.sep} ETA: {ETA}")
+            # calculate time
+            time_per_pair = get_average_diff([b for a, b in self.pairs])
+            time_left = self.pairs_left * time_per_pair
+            ETA = time.time() + time_left
+            if twenty_four == True:
+                ETA = datetime.datetime.fromtimestamp(ETA).strftime('%H:%M')
+            else:
+                ETA = datetime.datetime.fromtimestamp(ETA).strftime('%-I:%M %p')
+            
+            # if you took a break, start the dataset from scratch
+            if time_per_pair > 60:
+                self.pairs = []
+                self.updatePairlist(tool_group.message_box)
+            
+            # set the appropriate info
+            tool_group.message_box.set(f"Remaining: {self.pairs_left} {self.sep} Δ: {round(time_per_pair, 1)} sec. {self.sep} ETA: {ETA}")
         
         
-    def updatePairlist(self):
+    def updatePairlist(self, message_box):
 
         # store recent pairs & set message if not enough pairs in the dataset
         if not self.pair in [a for a, b in self.pairs]:
@@ -138,7 +157,7 @@ class KerningETA:
             if len(self.pairs) > self.dataset_size:
                 self.pairs.pop(0)
             else:
-                self.tool_group.message_box.set(f"Remaining: {self.pairs_left} {self.sep} Kern {self.dataset_size - len(self.pairs)} more pairs to get an ETA")
+                message_box.set(f"Remaining: {self.pairs_left} {self.sep} Kern {self.dataset_size - len(self.pairs)} more pairs to get an ETA")
                 return
         
 
