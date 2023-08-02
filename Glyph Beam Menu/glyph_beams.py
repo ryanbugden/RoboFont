556d7a09ceff5472dbcacd69cdf2c28a928f568d
Ryan Bugden <rbugden@gmail.com>
2023-08-01 23:32:05 -0400

    Update glyph_beams.py

--- Glyph Beam Menu/glyph_beams.py
+++ Glyph Beam Menu/glyph_beams.py
@@ -1,10 +1,8 @@
 # menuTitle : Glyph Beams
 
 from vanilla import FloatingWindow, PopUpButton
-from mojo.UI import CurrentGlyphWindow
-from lib.tools.defaults import getDefaultColor
+from mojo.UI import CurrentGlyphWindow, getDefault
 from mojo.subscriber import Subscriber, registerGlyphEditorSubscriber
-# from mojo.extensions import getExtensionDefault, setExtensionDefault
 
 '''
 Start-up script that adds a little menu under your Glyph View,
@@ -13,43 +11,47 @@ vertical position of which is determined by your Font's dimensions.
 Toggle the guides on and off.
 
 Ryan Bugden
-2019.04.02
+2023.08.01
 2022.02.23
+2019.04.02
 '''
 
-id_lib_key = 'com.ryanbugden.glyphBeams.beamIdentifiers'
-pref_lib_key = 'com.ryanbugden.glyphBeams.preferences'
+EXTENSION_KEY = "com.ryanbugden.glyphBeams"
+ID_LIB_KEY    = EXTENSION_KEY + ".beamIdentifiers"
+PREF_LIB_KEY  = EXTENSION_KEY + ".preferences"
+OOB_DISTANCE  = 100
+
 
 class GlyphBeams(Subscriber):
+
 
     def build(self):
         self.g = None
         self.f = CurrentFont()
-        col = getDefaultColor("spaceCenterBeamStrokeColor")
-        self.sel_color = (col.redComponent(), col.greenComponent(), col.blueComponent(), col.alphaComponent())
+        self.beam_color = tuple(getDefault("spaceCenterBeamStrokeColor"))
         self.title = 'Glyph Beams'
 
         try: 
-            # forcing the order of this dictionary
-            self.prefs = {'Uppercase': self.f.lib[pref_lib_key]['Uppercase'], 'Lowercase': self.f.lib[pref_lib_key]['Lowercase'], 'Plumb': self.f.lib[pref_lib_key]['Plumb']}
+            # Forcing the order of this dictionary
+            self.prefs = {'Uppercase': self.f.lib[PREF_LIB_KEY]['Uppercase'], 'Lowercase': self.f.lib[PREF_LIB_KEY]['Lowercase'], 'Plumb': self.f.lib[PREF_LIB_KEY]['Plumb']}
         except KeyError:
             self.prefs = {'Uppercase': False, 'Lowercase': False, 'Plumb': False} # starting point of the settings
-            self.storePrefs()
+            self.store_prefs()
 
-        self.width = 80 # width of the button
+        self.width = 82  # Width of the button
         
-        # store these ids in the extension, so they can be deleted on restart
+        # Store these IDs in the extension, so they can be deleted on restart.
         try: 
-            self.ids = self.f.lib[id_lib_key]
+            self.ids = self.f.lib[ID_LIB_KEY]
         except KeyError:
             self.ids = {'Uppercase': [], 'Lowercase': [], 'Plumb': []}
-            self.storeIDs()
+            self.store_ids()
 
-        # print("test_1 ", self.prefs)
             
     @property
     def window(self):
         return CurrentGlyphWindow()
+
 
     @property
     def bar(self):
@@ -57,11 +59,14 @@ class GlyphBeams(Subscriber):
             return
         return self.window.getGlyphStatusBar()
 
-    def storeIDs(self):
-        self.f.lib[id_lib_key] = self.ids
 
-    def storePrefs(self):
-        self.f.lib[pref_lib_key] = self.prefs
+    def store_ids(self):
+        self.f.lib[ID_LIB_KEY] = self.ids
+
+
+    def store_prefs(self):
+        self.f.lib[PREF_LIB_KEY] = self.prefs
+
 
     def glyphEditorDidOpen(self, info):
         # print("test_2 ", self.prefs)
@@ -84,45 +89,54 @@ class GlyphBeams(Subscriber):
             menuItem.setState_(value)
             # print("put menu items", i, menuItem, value)
 
-        self.addGuides()
+        self.add_guides()
         
             
     def glyphEditorGlyphDidChangeMetrics(self, info):
         # update vertical guide position
         self.g = info['glyph']
-        self.updatePlumb()
+        self.update_plumb()
+
                 
-    def roboFontDidSwitchCurrentGlyph(self, info):
+    def glyphEditorDidSetGlyph(self, info):
         # update vertical guide position
         self.g = info['glyph']
-        self.updatePlumb()
+        self.update_plumb()
 
-    def updatePlumb(self):
-        if self.g is not None:
+
+    def glyphEditorFontInfoDidChange(self, info):
+        self.update_horizontals()
+        self.update_plumb()
+
+
+    def update_plumb(self):
+        '''Updates the x-position and angle of the plumb guideline'''
+        if self.g:
             f = self.g.font
             for guideline in f.guidelines:
-                # print("roboFontDidSwitchCurrentGlyph LOOPING THROUGH GUIDELINES", self.g)
                 if guideline.identifier in self.ids['Plumb']:
-                    # print("roboFontDidSwitchCurrentGlyph CHANGE PLEASE", self.g)
                     offset = 0
                     if 'com.typemytype.robofont.italicSlantOffset' in f.lib.keys():
                         offset = f.lib['com.typemytype.robofont.italicSlantOffset']
                     guideline.x = self.g.width/2 + offset
-                    guideline.y = 0
+                    guideline.y = f.info.descender - OOB_DISTANCE
                     if f.info.italicAngle:
                         guideline.angle = 90 + f.info.italicAngle
+            self.g.changed()
+
+
+    def update_horizontals(self):
+        '''Updates the y-position of the uppercase and lowercase guidelines'''
+        if self.g:
+            f = self.g.font
+            for guideline in f.guidelines:
+                if guideline.identifier in self.ids['Uppercase']:
+                    guideline.y = f.info.capHeight / 2
+                if guideline.identifier in self.ids['Lowercase']:
+                    guideline.y = f.info.xHeight / 2
+            self.g.changed()
 
         
-    def clearGuides(self, font):
-        for guideline in font.guidelines:
-            for key in self.ids.keys():
-                if guideline.identifier in self.ids[key]:
-                    # remove cache of ids
-                    self.ids[key].remove(guideline.identifier)
-                    font.removeGuideline(guideline)
-
-                    self.storeIDs()
-                                
     def callback(self, sender):
         # print("test_3 ", self.prefs)
 
@@ -147,31 +161,43 @@ class GlyphBeams(Subscriber):
 
         # print("test_5 ", self.prefs)
 
-        self.storePrefs()
-        self.addGuides()
+        self.store_prefs()
+        self.add_guides()
 
 
-    def addGuides(self):    
+    def clear_guides(self, font):
+        for guideline in font.guidelines:
+            for key in self.ids.keys():
+                if guideline.identifier in self.ids[key]:
+                    # remove cache of ids
+                    self.ids[key].remove(guideline.identifier)
+                    font.removeGuideline(guideline)
+
+                    self.store_ids()
+
+
+    def add_guides(self):    
         f = CurrentFont()
-        self.clearGuides(f)
+        self.clear_guides(f)
         
         if self.prefs["Uppercase"] == True:
-            self.guideCap = f.appendGuideline(position=(0, int((f.info.capHeight)/2)), angle=0, color= self.sel_color)
-            self.guideCap.showMeasurements = True
-            self.guideCap.locked = True
-            self.ids['Uppercase'].append(self.guideCap.identifier)
+            self.guide_cap = f.appendGuideline(name="Uppercase", position=(-OOB_DISTANCE, int((f.info.capHeight)/2)), angle=0, color=self.beam_color)
+            self.guide_cap.showMeasurements = True
+            self.guide_cap.locked = True
+            self.ids['Uppercase'].append(self.guide_cap.identifier)
 
         if self.prefs["Lowercase"] == True:
-            self.guideX = f.appendGuideline(position=(0, int((f.info.xHeight)/2)), angle=0, color= self.sel_color)
-            self.guideX.showMeasurements = True
-            self.guideX.locked = True
-            self.ids['Lowercase'].append(self.guideX.identifier)
+            self.guide_x = f.appendGuideline(name="Lowercase", position=(-OOB_DISTANCE, int((f.info.xHeight)/2)), angle=0, color=self.beam_color)
+            self.guide_x.showMeasurements = True
+            self.guide_x.locked = True
+            self.ids['Lowercase'].append(self.guide_x.identifier)
 
         if self.prefs["Plumb"] == True:
-            self.guideVert = f.appendGuideline(position=(CurrentGlyph().width/2, 0), angle=90, color=self.sel_color)
-            self.guideVert.showMeasurements = True
-            self.guideVert.locked = True
-            self.ids['Plumb'].append(self.guideVert.identifier)
+            self.guide_vert = f.appendGuideline(name="Plumb", position=(CurrentGlyph().width/2, f.info.descender - OOB_DISTANCE), angle=90, color=self.beam_color)
+            self.guide_vert.showMeasurements = True
+            self.guide_vert.locked = True
+            self.ids['Plumb'].append(self.guide_vert.identifier)
+            self.update_plumb()
 
         # print("test_6 ", self.prefs)
             
@@ -179,7 +205,7 @@ class GlyphBeams(Subscriber):
             CurrentGlyph().changed()
         except:
             pass
-        self.storeIDs()
+        self.store_ids()
         
     
 registerGlyphEditorSubscriber(GlyphBeams)
